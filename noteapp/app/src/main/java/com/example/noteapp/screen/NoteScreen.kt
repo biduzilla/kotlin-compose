2300cef2387b8ea28c957be04bfcd450303f8f96
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-06-05 09:10:18 -0300

    itens notas

--- noteapp/app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
+++ noteapp/app/src/main/java/com/example/noteapp/screen/NoteScreen.kt
@@ -1,17 +1,21 @@
 package com.example.noteapp.screen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,9 +23,12 @@ import androidx.compose.ui.unit.dp
 import com.example.noteapp.R
 import com.example.noteapp.components.NoteButton
 import com.example.noteapp.components.NoteInputText
+import com.example.noteapp.data.NoteDataSource
 import com.example.noteapp.model.Note
+import java.time.format.DateTimeFormatter
 import kotlin.contracts.contract
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NoteScreen(notes: List<Note>, onAddNote: (Note) -> Unit, onRemoteNote: (Note) -> Unit) {
 
@@ -71,11 +78,47 @@ fun NoteScreen(notes: List<Note>, onAddNote: (Note) -> Unit, onRemoteNote: (Note
                 }
             })
         }
+        Divider(modifier = Modifier.padding(10.dp))
+        LazyColumn {
+            items(notes) {
+                NoteRow(note = it, onClicked = {})
+            }
+        }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemoteNote = {})
+    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {}, onRemoteNote = {})
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onClicked: (Note) -> Unit) {
+    Surface(
+        modifier = Modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 34.dp, bottomStart = 34.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB)
+    ) {
+        Column(
+            modifier
+                .clickable { }
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start) {
+            Text(text = note.title, style = MaterialTheme.typography.subtitle2)
+            Text(text = note.description, style = MaterialTheme.typography.subtitle1)
+            Text(
+                text = note.entryData.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.caption
+            )
+
+        }
+    }
 }
